7985aadcecfb4c1044f9cf700ebd9a2a608b927d
TejashGC <119960392+TejashGC@users.noreply.github.com>
2026-03-10 16:45:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,14 +14,37 @@ const elements=document.querySelectorAll(".reveal");
 const windowHeight=window.innerHeight;
 
 elements.forEach(el=>{
+
 const elementTop=el.getBoundingClientRect().top;
 
 if(elementTop<windowHeight-100){
 el.classList.add("active");
 }
+
 });
 
 }
 
 window.addEventListener("scroll",revealOnScroll);
 window.addEventListener("load",revealOnScroll);
+
+
+/* typing animation */
+
+const text="Electronics & Telecommunication Engineer";
+let index=0;
+
+function typing(){
+
+if(index<text.length){
+document.getElementById("typing").innerHTML+=text.charAt(index);
+index++;
+setTimeout(typing,50);
+}
+
+}
+
+window.onload=function(){
+typing();
+revealOnScroll();
+};
