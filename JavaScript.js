c0633d8f7640ba1b209f1b94108163b7c4a0403e
TejashGC <119960392+TejashGC@users.noreply.github.com>
2026-03-10 16:33:56 +0530

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,41 +1,27 @@
-particlesJS("particles-js", {
-
-particles: {
-
-number: { value: 60 },
-
-size: { value: 3 },
-
-color: { value: "#00e0ff" },
-
-line_linked: { enable: true, color: "#00e0ff" },
-
-move: { speed: 2 }
-
+particlesJS("particles-js",{
+particles:{
+number:{value:60},
+size:{value:3},
+color:{value:"#00e0ff"},
+line_linked:{enable:true,color:"#00e0ff"},
+move:{speed:2}
 }
-
 });
-
-const reveals = document.querySelectorAll(".reveal");
 
 function revealOnScroll(){
 
-const windowHeight = window.innerHeight;
+const elements=document.querySelectorAll(".reveal");
+const windowHeight=window.innerHeight;
 
-reveals.forEach(el => {
+elements.forEach(el=>{
+const elementTop=el.getBoundingClientRect().top;
 
-const elementTop = el.getBoundingClientRect().top;
-
-if(elementTop < windowHeight - 80){
-
+if(elementTop<windowHeight-100){
 el.classList.add("active");
-
 }
-
 });
 
 }
 
-window.addEventListener("scroll", revealOnScroll);
-
-window.addEventListener("load", revealOnScroll);
+window.addEventListener("scroll",revealOnScroll);
+window.addEventListener("load",revealOnScroll);
