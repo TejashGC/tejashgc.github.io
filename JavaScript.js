c1380a23138f08bd80f970aa06d2c8f4013087ae
TejashGC <119960392+TejashGC@users.noreply.github.com>
2026-03-10 16:16:26 +0530

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,39 +1,29 @@
 particlesJS("particles-js", {
-
-particles: {
-
-number: { value: 60 },
-
-size: { value: 3 },
-
-color: { value: "#00e0ff" },
-
-line_linked: { enable: true, color: "#00e0ff" },
-
-move: { speed: 2 }
-
-}
-
+  particles: {
+    number: { value: 60 },
+    size: { value: 3 },
+    color: { value: "#00e0ff" },
+    line_linked: { enable: true, color: "#00e0ff" },
+    move: { speed: 2 }
+  }
 });
 
 const reveals = document.querySelectorAll(".reveal");
 
 function revealOnScroll() {
+  const windowHeight = window.innerHeight;
 
-reveals.forEach(el => {
+  reveals.forEach(el => {
+    const elementTop = el.getBoundingClientRect().top;
 
-const rect = el.getBoundingClientRect();
-
-if (rect.top < window.innerHeight - 100) {
-
-el.classList.add("active");
-
-}
-
-});
-
+    if (elementTop < windowHeight - 80) {
+      el.classList.add("active");
+    }
+  });
 }
 
 window.addEventListener("scroll", revealOnScroll);
 
-window.addEventListener("load", revealOnScroll);
+window.addEventListener("load", () => {
+  revealOnScroll();
+});
