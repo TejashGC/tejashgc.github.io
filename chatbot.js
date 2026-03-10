eace51618533025184e9e41640e6902fb97069ad
TejashGC <119960392+TejashGC@users.noreply.github.com>
2026-03-10 22:10:58 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,5 +1,11 @@
+const icon=document.getElementById("chat-icon")
+const bot=document.getElementById("chatbot")
 const input=document.getElementById("chat-input")
 const body=document.getElementById("chat-body")
+
+icon.onclick=()=>{
+bot.style.display=bot.style.display==="block"?"none":"block"
+}
 
 input.addEventListener("keypress",e=>{
 
@@ -11,9 +17,11 @@ body.innerHTML+="<div>You: "+q+"</div>"
 
 let a="I am Tejash's portfolio assistant."
 
-if(q.includes("skills")) a="Tejash works on Embedded systems, IoT and communication."
+if(q.toLowerCase().includes("skills"))
+a="Tejash works on Embedded systems, IoT and communication."
 
-if(q.includes("projects")) a="He built LiFi, RF433 system and IoT asthma monitor."
+if(q.toLowerCase().includes("projects"))
+a="He built LiFi system, RF433 communication and IoT asthma monitoring."
 
 body.innerHTML+="<div>AI: "+a+"</div>"
 
